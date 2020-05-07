8e86985c444311ae57173dd51ba37e5271c195bc
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-05-07 16:54:54 -0700

    Adding memoization to fibonacci

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -9,19 +9,33 @@ fib(4) === 3 */
 
 // Iterative approach
 function fibIt(n){
-  const results= [0,1]
+  const results= [0,1];
   
-  for(let i=2; i<=n, i++){
-    const a= results[i-1]
-    const b= results[i-2]
-    results.push(a+b)
+  for(let i=2; i<=n; i++){
+    const a= results[i-1];
+    const b= results[i-2];
+    results.push(a+b);
   }
-  return results[results.length-1]
+  return results[results.length-1];
 }
 // Recursive approach
 function fib(n){
   if(n>2){
-    return n
+    return n;
   }
-  return fib(n-1) + fib(n-2)
+  return fib(n-1) + fib(n-2);
+}
+const fib= memoize(fib);
+
+// Adding memoization
+function memoize(fn){
+  const cache= {};
+  return function(...args){
+    if(cache[args]){
+      return cache[args];
+    }
+    const result= fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
 }
