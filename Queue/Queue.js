540a287f272ec1a4ea166c4f5c0d75f2c1155355
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-05-08 18:29:00 -0700

    Creating queue class

---
+++ Queue/Queue.js
@@ -0,0 +1,14 @@
+class Queue(){
+  constructor(){
+    this.data = [];
+  }
+  add(item){
+    this.data.unshift(item);
+  }
+  remove(){
+    return this.data.pop();
+  }
+  peek(){
+    return this.data[this.data.lenght-1]
+  }
+}
