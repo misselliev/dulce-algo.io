4e1c601464d6b214c7b7ce13f73f98fd6102e4f9
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-05-27 19:14:17 -0700

    Creating a Queue from stacks

---
+++ Queue from Stack/queueFromStack.js
@@ -0,0 +1,49 @@
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+const Stacks = require('../Stack/Stack');
+
+class Queue{
+  constructor(){
+    this.first= new Stack();
+    this.second = new Stack();
+  }
+
+  add(item){
+    this.first.push(item);
+  }
+
+  remove(){
+    while(this.first.peek()){
+      this.second.push(this.first.pop());
+    }
+    const item = this.second.pop();
+
+    while(this.second.peek()){
+      this.first.push(this.second.pop())
+    }
+    return item;
+  }
+
+  peek(){
+    while(this.first.peek()){
+      this.second.push(this.first.pop());
+    }
+    const item = this.second.peek();
+
+    while(this.second.peek()){
+      this.first.push(this.second.pop())
+    }
+    return item;
+  }
+}
