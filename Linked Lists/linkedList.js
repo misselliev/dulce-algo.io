1cdb2ffd0f6eb11499e0c1935bbf21e6aeea78df
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-06-01 17:09:43 -0700

    Creating a linkedlist constructor

---
+++ Linked Lists/linkedList.js
@@ -0,0 +1,7 @@
+// Create a class to represent a linked list.  When created, a linked list should have *no* head node associated with it. The LinkedList instance will have one property, 'head', which is a reference to the first node of the linked list. By default 'head' should be 'null'.
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+  }
+}
