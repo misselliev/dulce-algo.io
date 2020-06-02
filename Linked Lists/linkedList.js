970dfb821932ca72f8eb8d0bae54867b8bb77dd5
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-06-01 17:25:54 -0700

    Adding insertFirst and size methods to LinkedList class

--- Linked Lists/linkedList.js
+++ Linked Lists/linkedList.js
@@ -1,7 +1,28 @@
 // Create a class to represent a linked list.  When created, a linked list should have *no* head node associated with it. The LinkedList instance will have one property, 'head', which is a reference to the first node of the linked list. By default 'head' should be 'null'.
 
+// INSERT FIRST: Creates a new Node from argument 'data' and assigns the resulting node to the 'head' property. Make sure to handle the case in which the linked list already has a node assigned to the 'head' property.
+
+//  SIZE: Returns the number of nodes in the linked list.
+
+const Node = require('./node');
+
 class LinkedList{
   constructor(){
     this.head = null;
+  }
+
+  insertFirst(data){
+    this.head = new Node(data, this.head);
+  }
+
+  size(){
+    let counter = 0;
+    let node = this.head;
+
+    while(node){
+      counter++;
+      node = node.next;
+    }
+    return counter;
   }
 }
